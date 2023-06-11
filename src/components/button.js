4bce6f523e4bd14f3c519774ef8148b0fb3af100
zahra-abdulkarem <122061618+zahra-abdulkarem@users.noreply.github.com>
2023-06-11 04:59:10 +0300

    Delete src directory

--- src/components/button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-function Button(props) {
-    return (
-        <button className="btn">{props.children}<span>&#10230;</span></button>
-    )
-}
-
-export default Button
--- src/components/formatCurrency.js
+++
@@ -1,7 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {currency: "USD", style: "currency",});
-
-const formatCurrency = (number) =>{
-    return CURRENCY_FORMATTER.format(number);
-};
-
-export default formatCurrency;
